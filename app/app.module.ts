a62edba2cc5c0ca889e1fd1be40760611d764972
Andrew Tobilko <andrew.tobilko@gmail.com>
2016-10-22 23:03:16 +0300

    ts module + main component

---
+++ app/app.module.ts
@@ -0,0 +1,9 @@
+import {ApplicationComponent} from "./app.component";
+
+@NgModule({
+    declarations: [
+        ApplicationComponent
+    ],
+    bootstrap: [ApplicationComponent]
+})
+export class ApplicationModule {}
