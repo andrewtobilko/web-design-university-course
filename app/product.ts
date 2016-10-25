8ce2fbc97baa52c107db08271275463c30acc64b
Andrew Tobilko <andrew.tobilko@gmail.com>
2016-10-25 23:59:51 +0300

    iterated over products in main component

--- app/product.ts
+++ app/product.ts
@@ -1,13 +1,7 @@
 export class Product {
 
-    id: number;
-    title: string;
-    description: string;
-
-    constructor(id: number, title: string, description: string) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-    }
+    constructor(private id: number,
+                private title: string,
+                private description: string) {}
 
 }
