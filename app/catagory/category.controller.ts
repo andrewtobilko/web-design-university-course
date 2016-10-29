8a1abc776e38f206f487db57f71ca8572160b42a
Andrew Tobilko <andrew.tobilko@gmail.com>
2016-10-29 23:48:40 +0300

    category template

--- app/catagory/category.controller.ts
+++ app/catagory/category.controller.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from "@angular/core";
 import {CategoryService} from "./category.service";
+import {Product} from "../product/product.model";
 
 @Component({
     selector: 'category',
@@ -8,13 +9,18 @@ import {CategoryService} from "./category.service";
 })
 export class CategoryController implements OnInit {
 
+    private products: Product[];
+
     constructor(private categoryService: CategoryService) {}
 
     ngOnInit(): void {
-        /*this.categoryService.getProducts().then(
-            (data) => {},
+        this.categoryService.getProducts().then(
+            (data) => {
+                console.log(data)
+                this.products = data;
+            },
             (error) => {}
-        )*/
+        )
     }
 
 }
--- app/catagory/category.service.ts
+++ app/catagory/category.service.ts
@@ -1,4 +1,20 @@
 import {Injectable} from "@angular/core";
+import {Product} from "../product/product.model";
 
 @Injectable
-export class CategoryService {}
+export class CategoryService {
+
+    getProducts(): Promise<Product[]> {
+        return Promise.resolve([
+            new Product(0, "t1", "d1", 1, "m1"),
+            new Product(1, "t2", "d2", 2, "m2"),
+            new Product(2, "t3", "d3", 3, "m3"),
+            new Product(3, "t4", "d4", 4, "m4"),
+            new Product(4, "t5", "d5", 5, "m5"),
+            new Product(5, "t6", "d6", 6, "m6"),
+            new Product(6, "t7", "d7", 7, "m7"),
+            new Product(7, "t8", "d8", 8, "m8")
+        ])
+    }
+
+}
